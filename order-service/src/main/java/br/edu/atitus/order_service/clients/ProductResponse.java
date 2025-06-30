634a730f1825ab25350340c26370adaa5671ccf7
Mateus Lora <loramateusatitus@gmail.com>
2025-06-30 12:46:25 -0300

    fix: Order DTO

--- order-service/src/main/java/br/edu/atitus/order_service/clients/ProductResponse.java
+++ order-service/src/main/java/br/edu/atitus/order_service/clients/ProductResponse.java
@@ -1,14 +1,13 @@
 package br.edu.atitus.order_service.clients;
 
 public record ProductResponse(
-	    Long id,
-	    String description,
-	    String brand,
-	    String model,
-	    double price,
-	    String currency,
-	    Integer stock,
-	    String imageUrl,
-	    String enviroment,
-	    double convertedPrice
-	) {}
+		String theme,
+		Integer quantity,
+		String description,
+		String category,
+		double price,
+		String currency,
+		Integer stock,
+		String imageUrl){
+	
+}
